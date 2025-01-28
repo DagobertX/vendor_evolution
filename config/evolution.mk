562b672316d2a07a6950fe3ea959430a158dc7ec
minaripenguin <minaripenguin@users.noreply.github.com>
2025-01-28 20:27:05 +0900

    config: properties: Add pihooks default values
    * Android 16 QPR3 Beta 2
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: Joey <joey@evolution-x.org>

--- config/evolution.mk
+++ config/evolution.mk
@@ -1,3 +1,15 @@
+# PIF values
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.pihooks_MANUFACTURER?=Google \
+    persist.sys.pihooks_BRAND?=google \
+    persist.sys.pihooks_MODEL?=Pixel 9 Pro XL \
+    persist.sys.pihooks_FINGERPRINT?=google/komodo_beta/komodo:16/CP11.251209.007.A1/14691868:user/release-keys \
+    persist.sys.pihooks_PRODUCT?=komodo_beta \
+    persist.sys.pihooks_DEVICE?=komodo \
+    persist.sys.pihooks_ID?=CP11.251209.007.A1 \
+    persist.sys.pihooks_SECURITY_PATCH?=2026-01-05 \
+    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21
+
 # Evolution X packages
 PRODUCT_PACKAGES += \
     EvoEgg
