c23ba4133ad1179596ee69f4feee000074576365
Joel Lucas <JoelLucas@users.noreply.github.com>
2025-09-24 20:54:55 +0000

    config: Spoof BuildFingerprint as Pixel Canary
    * It is necessary to pass integrity with the built-in Keybox Importer, as our stock fingerprint is not "Valid"

--- config/evolution.mk
+++ config/evolution.mk
@@ -1,3 +1,7 @@
+# BuildFingerprint spoof to fix RCS/Wallet
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildFingerprint=google/mustang_beta/mustang:CANARY/ZP11.260515.009/15513807:user/release-keys
+
 # Evolution X packages
 PRODUCT_PACKAGES += \
     AxQuickLook \
